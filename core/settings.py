202216fc1de62bd8bc18872ec01454e8791ff1ed
Gagan Ahuja <gagahuja@yahoo.com>
2026-02-18 15:55:04 +0530

    Remove superuser logic from settings

--- core/settings.py
+++ core/settings.py
@@ -152,14 +152,5 @@ CSRF_TRUSTED_ORIGINS = [
 ]
 
 
-from django.contrib.auth import get_user_model
 
-if os.environ.get("RENDER") == "true":
-    User = get_user_model()
-    if not User.objects.filter(username="admin").exists():
-        User.objects.create_superuser(
-            username="admin",
-            email="admin@example.com",
-            password="admin123"
-        )
 
