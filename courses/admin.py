256653e1ddfa55039a83a59078791d7cefd11a64
Gagan Ahuja <gagahuja@yahoo.com>
2026-02-19 16:58:08 +0530

    Clean reset of courses migrations

--- courses/admin.py
+++ courses/admin.py
@@ -16,7 +16,7 @@ class CourseAdmin(admin.ModelAdmin):
 class ModuleAdmin(admin.ModelAdmin):
     list_display = ('title', 'course', 'order')
     list_filter = ('course',)
-    search_fields = ('title', 'course__title')
+    
 
 
 @admin.register(ClassSession)
