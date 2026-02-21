d15320f9769a46fe1ab194297f166d56860b986e
Gagan Ahuja <gagahuja@yahoo.com>
2026-02-21 17:01:31 +0530

    Added auto profile creation signal

--- courses/signals.py
+++ courses/signals.py
@@ -1,29 +1,40 @@
-import os
-from django.db.models.signals import post_migrate
-from django.contrib.auth import get_user_model
-from django.dispatch import receiver
+#import os
+#from django.db.models.signals import post_migrate
+#from django.contrib.auth import get_user_model
+#from django.dispatch import receiver
+#from django.db.models.signals import post_save
+#from django.contrib.auth.models import User
+#from .models import StudentProfile
+
+#@receiver(post_save, sender=User)
+#def create_profile(sender, instance, created, **kwargs):
+#    if created:
+#        StudentProfile.objects.create(user=instance)
+
+
+#@receiver(post_migrate)
+#def create_superuser(sender, **kwargs):
+#    username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+#    email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
+#    password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+
+#    if username and email and password:
+#        User = get_user_model()
+#        if not User.objects.filter(username=username).exists():
+#            User.objects.create_superuser(
+#                username=username,
+#                email=email,
+#                password=password
+#            )
+#            print("Superuser created successfully")
+
 from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.contrib.auth.models import User
 from .models import StudentProfile
 
+
 @receiver(post_save, sender=User)
-def create_profile(sender, instance, created, **kwargs):
+def create_student_profile(sender, instance, created, **kwargs):
     if created:
         StudentProfile.objects.create(user=instance)
-
-
-@receiver(post_migrate)
-def create_superuser(sender, **kwargs):
-    username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
-    email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
-    password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
-
-    if username and email and password:
-        User = get_user_model()
-        if not User.objects.filter(username=username).exists():
-            User.objects.create_superuser(
-                username=username,
-                email=email,
-                password=password
-            )
-            print("Superuser created successfully")
