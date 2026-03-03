62cc49d8be55b1f5ecacddafb3e82d231ea49f70
Gagan Ahuja <gagahuja@yahoo.com>
2026-03-03 19:00:41 +0530

    Add real-time join/leave tracking

--- courses/consumers.py
+++ courses/consumers.py
@@ -7,6 +7,7 @@ class ClassroomConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.session_id = self.scope['url_route']['kwargs']['session_id']
         self.room_group_name = f'classroom_{self.session_id}'
+        self.username = self.scope["user"].username
 
         await self.channel_layer.group_add(
             self.room_group_name,
@@ -15,7 +16,25 @@ class ClassroomConsumer(AsyncWebsocketConsumer):
 
         await self.accept()
 
+        # Notify others that user joined
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                "type": "user_join",
+                "username": self.username
+            }
+        )
+
     async def disconnect(self, close_code):
+
+        # Notify others that user left
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                "type": "user_leave",
+                "username": self.username
+            }
+        )
 
         await self.channel_layer.group_discard(
             self.room_group_name,
@@ -23,19 +42,32 @@ class ClassroomConsumer(AsyncWebsocketConsumer):
         )
 
     async def receive(self, text_data):
-
         data = json.loads(text_data)
 
         await self.channel_layer.group_send(
             self.room_group_name,
             {
-                'type': 'classroom_message',
-                'message': data
+                "type": "chat_message",
+                "username": data["username"],
+                "message": data["message"]
             }
         )
 
-    async def classroom_message(self, event):
+    async def chat_message(self, event):
+        await self.send(text_data=json.dumps({
+            "type": "chat",
+            "username": event["username"],
+            "message": event["message"]
+        }))
 
-        await self.send(
-            text_data=json.dumps(event['message'])
-        )
+    async def user_join(self, event):
+        await self.send(text_data=json.dumps({
+            "type": "join",
+            "username": event["username"]
+        }))
+
+    async def user_leave(self, event):
+        await self.send(text_data=json.dumps({
+            "type": "leave",
+            "username": event["username"]
+        }))
