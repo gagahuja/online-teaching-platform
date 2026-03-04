968d510ca671ab1490986475c9977c3fd6cef0cb
Gagan Ahuja <gagahuja@yahoo.com>
2026-03-04 12:47:58 +0530

    Fix classroom consumer with attendance and raise hand

--- courses/consumers.py
+++ courses/consumers.py
@@ -4,15 +4,13 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 
 class ClassroomConsumer(AsyncWebsocketConsumer):
 
-    # store active users per room
     active_users = {}
 
     async def connect(self):
         self.session_id = self.scope['url_route']['kwargs']['session_id']
-        self.room_group_name = f'classroom_{self.session_id}'
+        self.room_group_name = f"classroom_{self.session_id}"
         self.username = self.scope["user"].username
 
-        # join group
         await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
@@ -20,13 +18,11 @@ class ClassroomConsumer(AsyncWebsocketConsumer):
 
         await self.accept()
 
-        # add user to active list
         if self.room_group_name not in ClassroomConsumer.active_users:
             ClassroomConsumer.active_users[self.room_group_name] = []
 
         ClassroomConsumer.active_users[self.room_group_name].append(self.username)
 
-        # broadcast updated user list
         await self.channel_layer.group_send(
             self.room_group_name,
             {
@@ -37,12 +33,10 @@ class ClassroomConsumer(AsyncWebsocketConsumer):
 
     async def disconnect(self, close_code):
 
-        # remove user
         if self.room_group_name in ClassroomConsumer.active_users:
             if self.username in ClassroomConsumer.active_users[self.room_group_name]:
                 ClassroomConsumer.active_users[self.room_group_name].remove(self.username)
 
-        # broadcast updated list
         await self.channel_layer.group_send(
             self.room_group_name,
             {
@@ -59,23 +53,44 @@ class ClassroomConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         data = json.loads(text_data)
 
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                "type": "chat_message",
-                "username": data["username"],
-                "message": data["message"]
-            }
-        )
+        if data["type"] == "chat":
+
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    "type": "chat_message",
+                    "username": data["username"],
+                    "message": data["message"]
+                }
+            )
+
+        if data["type"] == "raise_hand":
+
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    "type": "hand_raised",
+                    "username": data["username"]
+                }
+            )
 
     async def chat_message(self, event):
+
         await self.send(text_data=json.dumps({
             "type": "chat",
             "username": event["username"],
             "message": event["message"]
         }))
 
+    async def hand_raised(self, event):
+
+        await self.send(text_data=json.dumps({
+            "type": "hand",
+            "username": event["username"]
+        }))
+
     async def attendance_update(self, event):
+
         await self.send(text_data=json.dumps({
             "type": "attendance",
             "users": event["users"]
