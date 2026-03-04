201ff8b05ada713de07ee083fd8f73b03a12dcda
Gagan Ahuja <gagahuja@yahoo.com>
2026-03-04 21:43:53 +0530

    Enable ASGI server for WebSockets

--- core/settings.py
+++ core/settings.py
@@ -168,9 +168,6 @@ import os
 
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [os.environ.get("REDIS_URL")],
-        },
-    },
+        "BACKEND": "channels.layers.InMemoryChannelLayer"
+    }
 }
