e43204b1b282c73c3ecf2e2c5b23fe0b94e94f23
Gagan Ahuja <gagahuja@yahoo.com>
2026-03-05 22:33:29 +0530

    Remove superuser creation from settings

--- core/settings.py
+++ core/settings.py
@@ -184,13 +184,3 @@ CHANNEL_LAYERS = {
 }
 
 
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
-
-if DJANGO_SUPERUSER_USERNAME and not User.objects.filter(username=DJANGO_SUPERUSER_USERNAME).exists():
-    User.objects.create_superuser(
-        username=DJANGO_SUPERUSER_USERNAME,
-        email=DJANGO_SUPERUSER_EMAIL,
-        password=DJANGO_SUPERUSER_PASSWORD,
-    )
