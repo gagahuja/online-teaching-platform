e878adef440fd4a64e34a29c1b5615edfda52544
Gagan Ahuja <gagahuja@yahoo.com>
2026-03-05 23:02:46 +0530

    Fix ASGI settings initialization

--- core/asgi.py
+++ core/asgi.py
@@ -8,21 +8,25 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
+
+from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from django.core.asgi import get_asgi_application
-from courses.routing import websocket_urlpatterns
+import courses.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+# VERY IMPORTANT
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
 django_asgi_app = get_asgi_application()
 
-application = ProtocolTypeRouter({
-    "http": django_asgi_app,
-
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            websocket_urlpatterns
-        )
-    ),
-})
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": AuthMiddlewareStack(
+            URLRouter(
+                courses.routing.websocket_urlpatterns
+            )
+        ),
+    }
+)
