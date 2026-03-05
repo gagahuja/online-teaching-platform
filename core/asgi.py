e5983c2036ad3c93c97e48816e66a7afce144ec6
Gagan Ahuja <gagahuja@yahoo.com>
2026-03-05 23:26:30 +0530

    Fix Django app registry for Channels

--- core/asgi.py
+++ core/asgi.py
@@ -8,8 +8,11 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
+import django
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+
+django.setup()
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
@@ -18,13 +21,11 @@ import courses.routing
 
 django_asgi_app = get_asgi_application()
 
-application = ProtocolTypeRouter(
-    {
-        "http": django_asgi_app,
-        "websocket": AuthMiddlewareStack(
-            URLRouter(
-                courses.routing.websocket_urlpatterns
-            )
-        ),
-    }
-)
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            courses.routing.websocket_urlpatterns
+        )
+    ),
+})
