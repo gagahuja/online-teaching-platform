9a65d2580ff3cc278cac6c0a3ad27be24f52efc9
Gagan Ahuja <gagahuja@yahoo.com>
2026-03-05 23:16:18 +0530

    Fix ASGI initialization order

--- core/asgi.py
+++ core/asgi.py
@@ -9,14 +9,12 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 
 import os
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import courses.routing
-
-
-# VERY IMPORTANT
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
 django_asgi_app = get_asgi_application()
 
